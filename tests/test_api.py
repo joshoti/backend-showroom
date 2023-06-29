1ae8411e32636cc40876d3338cb1ab84df7b8f52
Josh Oti <joshoti539@gmail.com>
2023-06-30 00:33:53 +0100

    Fix: app context when testing

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,7 +1,11 @@
+from flask.testing import FlaskClient
+
 from api.views import ApiHealthCheck
 
 
-def test_health_check():
+def test_health_check(test_client: FlaskClient):
+    """Fixture provides application context """
+    
     api_response = ApiHealthCheck.get()
     
     assert api_response.status_code == 200
